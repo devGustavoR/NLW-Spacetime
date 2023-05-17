47be58369e2166ddb1d58037bd3f0b676237dca4
devGustavoR <devgustavor@gmail.com>
2023-05-17 16:21:51 -0300

    Parte Web feita e fontes importadas no Mobile

--- Mobile/App.tsx
+++ Mobile/App.tsx
@@ -1,10 +1,31 @@
 import { StatusBar } from 'expo-status-bar'
 import { Text, View } from 'react-native'
 
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts
+} from '@expo-google-fonts/roboto'
+
+import {
+  BaiJamjuree_700Bold
+} from '@expo-google-fonts/bai-jamjuree'
+
+
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold
+  })
+
+  if (!hasLoadedFonts) {
+    return null
+  }
+
   return (
-    <View className='bg-gray-950 flex-1 justify-center items-center'>
-      <Text className='text-gray-50 font-bold text-5xl'>Hello World</Text>
+    <View className='bg-gray-900 flex-1 justify-center items-center'>
+      <Text className='text-gray-50 font-bold text-5xl font-title'>Hello World</Text>
       <StatusBar style="light" translucent />
     </View>
   )
