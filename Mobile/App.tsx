4fdac2c06f22c6a8930c887d8027d801fcdb4a37
devGustavoR <devgustavor@gmail.com>
2023-05-17 21:21:01 -0300

    Mobile pronta

--- Mobile/App.tsx
+++ Mobile/App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar'
-import { Text, View } from 'react-native'
+import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
 
 import {
   Roboto_400Regular,
@@ -10,6 +10,13 @@ import {
 import {
   BaiJamjuree_700Bold
 } from '@expo-google-fonts/bai-jamjuree'
+
+import blurBg from './src/assets/bg-blur.png'
+import Stripes from './src/assets/stripes.svg'
+import NLW from './src/assets/nlw-spacetime-logo.svg'
+import { styled } from 'nativewind'
+
+const StyledStripes = styled(Stripes)
 
 
 export default function App() {
@@ -24,10 +31,30 @@ export default function App() {
   }
 
   return (
-    <View className='bg-gray-900 flex-1 justify-center items-center'>
-      <Text className='text-gray-50 font-bold text-5xl font-title'>Hello World</Text>
+    <>
+    <ImageBackground 
+    source={blurBg} 
+    className='bg-gray-900 flex-1 px-8 py-10 items-center'
+    imageStyle={{ position:'absolute', left:'-100%'}}>
+      <StyledStripes className='absolute left-1' />
+
+      <View className='flex-1 justify-center items-center gap-6'>
+        <NLW/>
+
+        <View className='space-y-3'>
+          <Text className='text-center font-title text-2xl leading-tight text-gray-50'>Sua cápsula do tempo</Text>
+          <Text className='text-center font-body text-base leading-relaxed text-gray-100'>Colecione momentos marcantes da sua jornada e compartilhe (se quiser) com o mundo!</Text>
+        
+          <TouchableOpacity activeOpacity={0.7} className='rounded-full bg-green-500 px-5 py-3'>
+            <Text className='font-alt text-sm uppercase text-black text-center'>Cadastrar lembrança</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      <Text className='text-center font-body text-sm leading-relaxed text-gray-200'>Feito com 💜 por devGustavoR</Text>
       <StatusBar style="light" translucent />
-    </View>
+    </ImageBackground>
+    </>
   )
 }
 
