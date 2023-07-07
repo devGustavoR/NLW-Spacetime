d7b0a66c96fe1eefee3912c17ab0ae4e8b520975
devGustavoR <devgustavor@gmail.com>
2023-07-07 15:36:13 -0300

    Back-end em andamento

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,32 +1,28 @@
 import { FastifyInstance } from 'fastify'
-import { z } from 'zod'
 
 export async function authRoutes(app: FastifyInstance) {
-  app.post('./register', async (request) => {
-    const bodySchema = z.object({
-      code: z.string(),
-    })
-    // const { code } = bodySchema.parse(request.body)
-
-    // const accessTokenResponde = await axios.post(
-    //   'https://github.com/login/oauth/access_token',
-    //   null,
-    //   {
-    //     params: {
-    //       code,
-    //       client_id: process.env.GITHUB_CLIENT_ID,
-    //       client_secret: process.env.GITHUB_CLIENT_SECRET,
-    //     },
-    //     headers: {
-    //       Accept: 'application/json',
-    //     },
-    //   },
-    // )
-
-    // const { access_token } = accessTokenResponde.data
-
-    // return {
-    //   access_token,
-    // }
-  })
+  // app.post('./register', async (request) => {
+  //   const bodySchema = z.object({
+  //     code: z.string(),
+  //   })
+  //   const { code } = bodySchema.parse(request.body)
+  //   const accessTokenResponde = await axios.post(
+  //     'https://github.com/login/oauth/access_token',
+  //     null,
+  //     {
+  //       params: {
+  //         code,
+  //         client_id: process.env.GITHUB_CLIENT_ID,
+  //         client_secret: process.env.GITHUB_CLIENT_SECRET,
+  //       },
+  //       headers: {
+  //         Accept: 'application/json',
+  //       },
+  //     },
+  //   )
+  //   // const { access_token } = accessTokenResponde.data
+  //   // return {
+  //   //   access_token,
+  //   // }
+  // })
 }
