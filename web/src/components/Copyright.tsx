db9c79daac3b6ec1b409f1c3febf66bd514efce1
devGustavoR <devgustavor@gmail.com>
2023-07-07 13:55:08 -0300

    Oauth pronto

---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,15 @@
+export function Copyright(){
+  return(
+    <div className="text-sm leading-relaxed text-gray-200">
+          Feito com 💜 no NLW da Rocketseat por{' '}
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/devGustavoR"
+            className="underline hover:text-orange-500"
+          >
+            devGustavor
+          </a>
+        </div>
+  )
+}
