3edb4123859ee0ec228740b44c2886a4b18eede8
devGustavoR <devgustavor@gmail.com>
2023-07-20 17:46:06 -0300

    Front-End em andamento

--- web/src/app/api/auth/callback/routes.ts
+++ web/src/app/api/auth/callback/routes.ts
@@ -1,5 +1,22 @@
-import { NextRequest } from "next/server";
+import api from "@/lib/api";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
-  console.log("oi")
+  const {searchParams} = new URL(request.url)
+
+  const code = searchParams.get('code')
+
+  const registerResponse = await api.post('/register',{
+    code,
+  })
+
+  const {token} = registerResponse.data
+
+  const redirectUrl = new URL('/', request.url)
+
+  return NextResponse.redirect(redirectUrl,{
+    headers: {
+      'Set-Cookie': `token=${token}; Path=/;`,
+    },
+  })
 }
