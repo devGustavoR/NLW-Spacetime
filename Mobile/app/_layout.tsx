7c5d990429e3777d890d49b0ab8d14118b50292b
devGustavoR - Trampo <devgustavor@gmail.com>
2023-08-21 17:34:44 -0300

    Mobile in progress

---
+++ Mobile/app/_layout.tsx
@@ -0,0 +1,50 @@
+import { styled } from "nativewind";
+import { ImageBackground } from "react-native";
+import blurBg from '../src/assets/bg-blur.png';
+import Stripes from '../src/assets/stripes.svg';
+
+import {
+  BaiJamjuree_700Bold
+} from '@expo-google-fonts/bai-jamjuree';
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts
+} from '@expo-google-fonts/roboto';
+import { SplashScreen, Stack } from "expo-router";
+import * as SecureStore from 'expo-secure-store';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+
+const StyledStripes = styled(Stripes)
+
+export default function Layout(){
+  const [isUserAuthenticated, setIsUserAuthenticated] = useState<null | boolean>(null,)
+
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold
+  })
+
+  useEffect({} => {
+    SecureStore.getItem
+  },[])
+
+  if (!hasLoadedFonts) {
+    return <SplashScreen/>
+  }
+  return(
+    <ImageBackground 
+    source={blurBg} 
+    className='bg-gray-900 flex-1'
+    imageStyle={{ position:'absolute', left:'-100%'}}>
+      <StyledStripes className='absolute left-1' />
+      <StatusBar style="light" translucent />
+      <Stack screenOptions={{
+        headerShown: false,
+        contentStyle:{backgroundColor:'transparent'},
+      }}/>
+      </ImageBackground>
+  )
+}
--- Mobile/app/index.tsx
+++ Mobile/app/index.tsx
@@ -1,27 +1,16 @@
 // Importações React, Expo, Expo-Google, 
-import {
-  BaiJamjuree_700Bold
-} from '@expo-google-fonts/bai-jamjuree';
-import {
-  Roboto_400Regular,
-  Roboto_700Bold,
-  useFonts
-} from '@expo-google-fonts/roboto';
 import { makeRedirectUri, useAuthRequest } from 'expo-auth-session';
 import { useRouter } from 'expo-router';
 import * as SecureStore from 'expo-secure-store';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
-import { ImageBackground, Text, TouchableOpacity, View } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
  
 // Importações Images
-import { styled } from 'nativewind';
-import blurBg from '../src/assets/bg-blur.png';
+
 import NLW from '../src/assets/nlw-spacetime-logo.svg';
-import Stripes from '../src/assets/stripes.svg';
 import { api } from '../src/lib/api';
 
-const StyledStripes = styled(Stripes)
 
 const discovery = {
   authorizationEndpoint: 'https://github.com/login/oauth/authorize',
@@ -67,23 +56,12 @@ export default function App() {
     }
   }, [response])
 
-  const [hasLoadedFonts] = useFonts({
-    Roboto_400Regular,
-    Roboto_700Bold,
-    BaiJamjuree_700Bold
-  })
-
-  if (!hasLoadedFonts) {
-    return null
-  }
+  
 
   return (
     <>
-    <ImageBackground 
-    source={blurBg} 
-    className='bg-gray-900 flex-1 px-8 py-10 items-center'
-    imageStyle={{ position:'absolute', left:'-100%'}}>
-      <StyledStripes className='absolute left-1' />
+    <View 
+    className='flex-1 px-8 py-10 items-center'>
 
       <View className='flex-1 justify-center items-center gap-6'>
         <NLW/>
@@ -100,7 +78,7 @@ export default function App() {
 
       <Text className='text-center font-body text-sm leading-relaxed text-gray-200'>Feito com 💜 no NLW da Rocketseat por devGustavoR</Text>
       <StatusBar style="light" translucent />
-    </ImageBackground>
+    </View>
     </>
   )
 }
